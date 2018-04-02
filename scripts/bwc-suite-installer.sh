4119f436f47929e905514d96bf6a570c9c965b0d
Warren Van Winckel <warren.van.winckel@gmail.com>
2018-04-01 23:06:03 -0700

    Redact password and license key from output

--- scripts/bwc-suite-installer.sh
+++ scripts/bwc-suite-installer.sh
@@ -162,6 +162,6 @@ else
     chmod +x ${SUITE_OS_INSTALLER_FILE}
 
     echo "Running deployment script for Brocade Workflow Composer ${VERSION}..."
-    echo "OS specific script cmd: bash ${SUITE_OS_INSTALLER_FILE} ${SUITE_VERSION} ${RELEASE} ${REPO_TYPE} ${USERNAME} ${PASSWORD} ${LICENSE_KEY}"
+    echo "OS specific script cmd: bash ${SUITE_OS_INSTALLER_FILE} ${SUITE_VERSION} ${RELEASE} ${REPO_TYPE} ${USERNAME} --password=**** --license=****"
     bash ${SUITE_OS_INSTALLER_FILE} ${SUITE_VERSION} ${RELEASE} ${REPO_TYPE} ${USERNAME} ${PASSWORD} ${LICENSE_KEY}
 fi
